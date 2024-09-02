5053999159deb6652d490044c850ff8fb1ac70c6
Muhammad Shahab Saqib <shahabsaqib220@gmail.com>
2024-09-02 07:51:03 -0700

    Link has bee setup

--- client/src/Components/Ribbon.jsx
+++ client/src/Components/Ribbon.jsx
@@ -11,7 +11,7 @@ const handleOrderNowClick = (e) => {
     behavior: "smooth", // Smooth scroll to the top
   });
   // If you need to redirect to the homepage, you can use:
-  window.location.href = "https://uk-assignmnet-project.vercel.app/";
+  window.location.href = "https://uk-assignmnet-project-frontend.vercel.app/";
 };
 const handleWhatappClick = () => {
   // Construct the WhatsApp URL with the phone number
