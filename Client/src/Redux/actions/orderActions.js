67e506e4227a57da6dfb6d6d2e074952aafae659
shahabsaqib220 <74920149+shahabsaqib220@users.noreply.github.com>
2024-09-02 03:42:31 -0700

    Delete Client directory

--- Client/src/Redux/actions/orderActions.js
+++
@@ -1,6 +0,0 @@
-export const SAVE_ORDER_DETAILS = 'SAVE_ORDER_DETAILS';
-
-export const saveOrderDetails = (orderDetails) => ({
-  type: SAVE_ORDER_DETAILS,
-  payload: orderDetails,
-});
--- Client/src/Redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import orderReducer from "./orderReducer";
-
-const rootReducer = combineReducers({
-  order: orderReducer,
-  // other reducers...
-});
-
-export default rootReducer;
